e84508346b4298e41c1fef3ab0be3a0d9a8d6f33
Seveshy <danielalvesmajor@outlook.com>
2021-06-27 10:49:49 -0300

    Page Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,30 +2,33 @@ import illustrationImg from '../assets/illustration.svg';
 import googleIconImg from '../assets/google-icon.svg';
 import logoImg from '../assets/logo.svg';
 
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
+
 export function Home() {
     return (
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp; A ao-vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button>
+                    <button className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
-                    <div>ou entre em uma sala</div>
+                    <div className="separetor">ou entre em uma sala</div>
                     <form action="">
                         <input 
                             type="text"
                             placeholder="Digite o código da sala"
                         />
-                        <button type="submit">
+                        <Button type="submit">
                             Entrar sala
-                        </button>
+                        </Button>
                     </form>
                 </div>
             </main>
