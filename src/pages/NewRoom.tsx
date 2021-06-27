ecba8fca420566d2f66939d61dafbe30ba73c685
Seveshy <danielalvesmajor@outlook.com>
2021-06-27 11:15:15 -0300

    Page NewRoom

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,19 +1,11 @@
-import { useHistory } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 import illustrationImg from '../assets/illustration.svg';
-import googleIconImg from '../assets/google-icon.svg';
 import logoImg from '../assets/logo.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
-export function Home() {
-    const history = useHistory();
-
-    function navigateToNewRoom() {
-        history.push('/rooms/news');
-    }
-
+export function NewRoom() {
     return (
         <div id="page-auth">
             <aside>
@@ -24,20 +16,19 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
-                        <img src={googleIconImg} alt="Logo do Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separetor">ou entre em uma sala</div>
+                    <h2>Criar uma nova sala</h2>
                     <form action="">
                         <input 
                             type="text"
-                            placeholder="Digite o código da sala"
+                            placeholder="Nome da sala"
                         />
                         <Button type="submit">
-                            Entrar sala
+                            Criar sala
                         </Button>
                     </form>
+                    <p>
+                        Quentrar em uma sala existente? <Link to="/">Clique aqui</Link>
+                    </p>
                 </div>
             </main>
         </div>
